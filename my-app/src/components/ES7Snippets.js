7695b7175ff6849800d6fc4d1d1e1b01e32d2489
ChrisKellFSD <chriskellcsn@gmail.com>
2023-01-12 12:19:00 +0000

    Add notes to the ES7 snippets

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,59 @@
+// * rfce: Creates a React Functional Component
+
+// import React from 'react'
+
+// function ES7Snippets() {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// ------------------------
+
+// * rafce: Creates a React Arrow Function Component
+
+// export default ES7Snippets
+
+// import React from 'react'
+
+// const ES7Snippets = () => {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// ------------------------
+
+// * rce: Creates a React class component
+
+// export default ES7Snippets
+
+// import React, { Component } from 'react'
+
+// export class ES7Snippets extends Component {
+//   render() {
+//     return (
+//       <div>ES7Snippets</div>
+//     )
+//   }
+// }
+
+// export default ES7Snippets
+
+// ------------------------
+
+// *rconst: Adds a default constructor for a class that contains props as an argument
+
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+
+// ------------------------
+
+// console.log(first) - * clg
+
+// console.log('first', first) - - * clo

31a78c294f502196478650eeb34f997440edd589
ChrisKellFSD <chriskellcsn@gmail.com>
2023-01-22 14:44:41 +0000

    Work on filtering search results by implementing handleChange functions

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
-import ControlledForm from "./components/ControlledForm";
-import UncontrolledForm from "./components/UncontrolledForm";
+import SearchBar from "./components/SearchBar";
+
 
 function App() {
   return <div className="App">
-      <ControlledForm />
-      <UncontrolledForm />
+      <SearchBar />
   </div>;
 }
 
